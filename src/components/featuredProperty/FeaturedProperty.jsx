2e59557e207a8b0ae623bd22e31beef431f63716
AssanRabari <assandesai@gmail.com>
2023-10-03 18:44:28 +0530

    featured and mailList components design

---
+++ src/components/featuredProperty/FeaturedProperty.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import "./featuredProperty.css";
+
+function FeaturedProperty() {
+    return (
+        <div className="fp">
+            <div className="fpItem">
+
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=35b70a7e8a17a71896996cd55d84f742cd15724c3aebaed0d9b5ba19c53c430b&o=" alt="" className="fpImg" />
+                <span className="fpName">Broad Lands</span>
+                <span className="fpCity">Chennai</span>
+                <span className="fpPrice">Starting from INR-5000</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+            <div className="fpItem">
+
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/199787573.webp?k=13aac32349703e05229b176c0e22e93c2daa986fcebd78fadb6ca47fe22bd212&o=" alt="" className="fpImg" />
+                <span className="fpName">Broad Lands</span>
+                <span className="fpCity">Chennai</span>
+                <span className="fpPrice">Starting from INR-5000</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+
+            <div className="fpItem">
+
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/352170812.webp?k=4ff5e29f3ad72c2c9f7170f60a043f01a158f26b38c55e9676439c18f3804179&o=" alt="" className="fpImg" />
+                <span className="fpName">Broad Lands</span>
+                <span className="fpCity">Chennai</span>
+                <span className="fpPrice">Starting from INR-5000</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+            <div className="fpItem">
+
+                <img src="https://cf.bstatic.com/xdata/images/hotel/square600/352170812.webp?k=4ff5e29f3ad72c2c9f7170f60a043f01a158f26b38c55e9676439c18f3804179&o=" alt="" className="fpImg" />
+                <span className="fpName">Broad Lands</span>
+                <span className="fpCity">Chennai</span>
+                <span className="fpPrice">Starting from INR-5000</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FeaturedProperty
